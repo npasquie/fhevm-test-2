a7a116073ffe6e2a69edab8134c57ff22058c33f
tobou <npasquie@users.noreply.github.com>
2024-11-11 11:39:36 +0700

    wip non naive implem

--- contracts/YaoPrivate.sol
+++ contracts/YaoPrivate.sol
@@ -4,63 +4,54 @@ pragma solidity ^0.8.24;
 import "fhevm/lib/TFHE.sol";
 import "fhevm/gateway/GatewayCaller.sol";
 
-/// @notice Yao's millionaire problem FHEVM implementation
-/// finds who is the richest among Alice, Bob, and Carol
+/// @notice Yao's millionaire problem FHEVM implementation, finds who is the richest participant
 contract YaoPrivate is GatewayCaller {
-    address immutable alice;
-    address immutable bob;
-    address immutable carol;
+    mapping(address => bool) isParticipating;
+    mapping(address => bool) hasSubmited;
+    uint256 nbOfParticipants;
+    uint256 nbOfSubmissions;
     euint64 highestWealth;
-    address public richest;
+    eaddress currentRichest;
+    address public richest; // 0 if the session richest is not yet decided
 
-    /// @dev this contract accepets 3 user but the same logic can be used
-    /// for an arbitrary number of users.
-    constructor(address _alice, address _bob, address _carol) {
-        alice = _alice;
-        bob = _bob;
-        carol = _carol;
-        highestWealth = TFHE.asEuint64(0);
-        TFHE.allow(highestWealth, address(this));
-    }
-
-    /// @notice Privately evaluates the wealth of the user and asynchronoulsy
-    /// updates the richest user if needed.
-    /// @dev alternatlively, wealth submission could be done through an encrypted erc20.
-    /// since the standard does not have a method to share encrypted balances
-    /// with another contract, we would have to approve, then transfer the full
-    /// wealth of the user to this contract, which would save its encrypted balance
-    /// difference before and after the transfer, then transfer the full amount back
-    /// to the user, then use this value to determine teh richest user.
-    function submitWealth(einput _wealth, bytes calldata inputProof) external {
-        require(
-            msg.sender == alice || msg.sender == bob || msg.sender == carol,
-            "Only Alice, Bob, and Carol can submit wealth"
-        );
-
-        euint64 wealth = TFHE.asEuint64(_wealth, inputProof);
-
-        ebool isTheWealthiest = TFHE.gt(wealth, highestWealth);
-        highestWealth = TFHE.select(isTheWealthiest, wealth, highestWealth);
-        TFHE.allow(highestWealth, address(this));
-
-        uint256[] memory cts = new uint256[](1);
-        cts[0] = Gateway.toUint256(isTheWealthiest);
-        uint256 requestID = Gateway.requestDecryption(
-            cts,
-            this.isTheWealthiestDecryptionCallback.selector,
-            0,
-            block.timestamp + 100,
-            false
-        );
-        addParamsAddress(requestID, msg.sender);
-    }
-
-    /// @notice Callback reserved for the gateway
-    function isTheWealthiestDecryptionCallback(uint256 requestID, bool decryptedIsTheWealthiest) external onlyGateway {
-        address[] memory sender = new address[](1);
-        sender = getParamsAddress(requestID);
-        if (decryptedIsTheWealthiest) {
-            richest = sender[0];
+    constructor(address[] memory participants) {
+        nbOfParticipants = participants.length;
+        for (uint256 i = 0; i < nbOfParticipants; i++) {
+            isParticipating[participants[i]] = true;
         }
+        highestWealth = TFHE.asEuint64(0);
+        currentRichest = TFHE.asEaddress(address(0));
+    }
+
+    /// @notice Privately evaluates the wealth of the caller, updates richest user and highest wealth
+    function submitWealth(einput _wealth, bytes calldata inputProof) external {
+        require(isParticipating[msg.sender], "You are not allowed to participate in this contest");
+        require(!hasSubmited[msg.sender], "You have already submitted your wealth");
+
+        euint64 callersWealth = TFHE.asEuint64(_wealth, inputProof);
+
+        ebool callerIsTheWealthiest = TFHE.gt(callersWealth, highestWealth);
+        highestWealth = TFHE.select(callerIsTheWealthiest, callersWealth, highestWealth);
+        currentRichest = TFHE.select(callerIsTheWealthiest, TFHE.asEaddress(msg.sender), currentRichest);
+
+        nbOfSubmissions++;
+        hasSubmited[msg.sender] = true;
+
+        decryptRichestOnLastSubmission();
+    }
+
+    function decryptRichestOnLastSubmission() internal {
+        if (nbOfSubmissions != nbOfParticipants) {
+            return;
+        }
+
+        TFHE.allow(currentRichest, address(this));
+        uint256[] memory cts = new uint256[](1);
+        cts[0] = Gateway.toUint256(currentRichest);
+        Gateway.requestDecryption(cts, this.richestDecryptionCallback.selector, 0, block.timestamp + 100, false);
+    }
+
+    function richestDecryptionCallback(uint256, address decryptedRichest) external onlyGateway {
+        richest = decryptedRichest;
     }
 }
