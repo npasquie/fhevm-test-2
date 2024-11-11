6ccbae717487946b19612341fb18079ad148c4ec
tobou <npasquie@users.noreply.github.com>
2024-11-11 21:38:42 +0700

    wip unit testing

--- test/yao/yaoPrivate.ts
+++ test/yao/yaoPrivate.ts
@@ -5,6 +5,7 @@ import type { YaoPrivate } from "../../types";
 import { awaitAllDecryptionResults } from "../asyncDecrypt";
 import { createInstances } from "../instance";
 import { getSigners, initSigners } from "../signers";
+import type { FhevmInstances } from "../types";
 
 interface YaoPrivateContext {
   yao: YaoPrivate;
@@ -25,8 +26,17 @@ describe("TestYaoPrivate", function () {
   });
 
   it("should find the richest user", async function () {
-    const input = this.instances.alice.createEncryptedInput(this.contractAddress, this.signers.alice.address);
-    input.add64(100);
+    submitWealthWith(this.instances.bob, 200);
+    submitWealthWith(this.instances.alice, 100);
+    const richest = await ctx.yao["richest"]();
+    console.log(richest);
+    console.log(this.signers.bob.address);
+    expect(richest).to.equal(this.signers.bob.address);
+  });
+
+  async function submitWealthWith(user: FhevmInstances[keyof FhevmInstances], wealthValue: number) {
+    const input = user.createEncryptedInput(ctx.yao.getAddress(), user.address);
+    input.add64(wealthValue);
     const encryptedWealthSubmission = input.encrypt();
     const tx = await ctx.yao["submitWealth"](
       encryptedWealthSubmission.handles[0],
@@ -34,20 +44,14 @@ describe("TestYaoPrivate", function () {
     );
     await tx.wait();
     await awaitAllDecryptionResults();
-    const richest = await ctx.yao["richest"]();
-    expect(richest).to.equal(this.signers.alice.address);
-
-    // I also tested this contract using Remix with multiple scenarios
-  });
+  }
 });
 
 async function deployYaoPrivateFixture(): Promise<YaoPrivate> {
   const signers = await getSigners();
 
   const contractFactory = await ethers.getContractFactory("YaoPrivate");
-  const contract = await contractFactory
-    .connect(signers.alice)
-    .deploy(signers.alice.address, signers.bob.address, signers.carol.address);
+  const contract = await contractFactory.connect(signers.alice).deploy([signers.alice.address, signers.bob.address]);
   await contract.waitForDeployment();
 
   return contract;
